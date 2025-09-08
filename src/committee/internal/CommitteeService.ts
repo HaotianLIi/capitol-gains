058dd933a9e4d6a08319dd3e69c954b5fef18b26
pl <plrtn@protonmail.com>
2025-09-08 11:33:39 -0400

    feat: change file ref to relative to where cmd is run (which should always be project root). Add legislator data

--- src/committee/internal/CommitteeService.ts
+++ src/committee/internal/CommitteeService.ts
@@ -35,13 +35,52 @@ const committeeSchema =  z.object({
     subcommittees: z.array(subCommitteeSchema).optional()
 })
 
+const legislatorSchema = z.object({
+    id: z.object({
+        bioguide: z.string(),
+        thomas: z.string().optional(),
+        lis: z.string().optional(),
+        govtrack: z.coerce.string(),
+        opensecrets: z.string().optional(),
+        votesmart: z.coerce.string(),
+        fec: z.string().array().optional(),
+        cspan: z.coerce.string(),
+    }),
+    name: z.object({
+        first:  z.string(),
+        last: z.string(),
+        official_full: z.string(),
+    }),
+    bio: z.object({
+        birthDay: z.date().optional(),
+        gender: z.enum(["M", "F"]),
+    }),
+    terms: z.object({
+        type: z.string(),
+        start: z.coerce.date(),
+        end: z.coerce.date(),
+        state: z.string().length(2),
+        district: z.number().int().optional(),
+        party: z.string(),
+        state_rank: z.string().optional(),
+        url: z.url().optional(),
+        address: z.string().optional(),
+        phone: z.string().optional(),
+        fax: z.string().nullable().optional(),
+        contact_form: z.string().optional(),
+        office: z.string().optional(),
+    }).array(),
+})
+
 type CommitteeMember = z.infer<typeof committeeMemberSchema>
 type CommitteeMembership = z.infer<typeof committeeMembershipSchema>
 type Committee = z.infer<typeof committeeSchema>
 type SubCommittee = z.infer<typeof subCommitteeSchema>
+type Legislator = z.infer<typeof legislatorSchema>
 
-const COMMITTEE_MEMBERSHIP_CURRENT_PATH = "congress-legislators/committee-membership-current.yaml"
-const COMMITTEE_CURRENT_PATH = "congress-legislators/committee-current.yaml"
+const COMMITTEE_MEMBERSHIP_CURRENT_PATH = process.cwd() + "/congress-legislators/committee-membership-current.yaml"
+const COMMITTEE_CURRENT_PATH = process.cwd() + "/congress-legislators/committees-current.yaml"
+const LEGISLATORS_CURRENT_PATH = process.cwd() + "/congress-legislators/legislators-current.yaml"
 
 async function getCommitteeMembershipData() : Promise<CommitteeMembership> {
     const data  = await Bun.file(COMMITTEE_MEMBERSHIP_CURRENT_PATH).text()
@@ -55,8 +94,16 @@ async function getCommitteeData() : Promise<Committee[]> {
     return Bun.YAML.parse(data).map(committeeSchema.parse)
 }
 
+async function getLegislatorData() : Promise<Legislator[]> {
+    const data = await Bun.file(LEGISLATORS_CURRENT_PATH).text()
+    // @ts-ignore
+    return Bun.YAML.parse(data).map(legislatorSchema.parse)
+}
+
+
 export const committeeData = await getCommitteeData()
 export const committeeMembershipData = await getCommitteeMembershipData()
+export const legislatorData = await getLegislatorData()
 
 export type {
     CommitteeMember,

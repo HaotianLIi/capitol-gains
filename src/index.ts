1af705f0c9413fcc0cd3b2a25ec8ff8b8f7b00ce
Haotian Li <frankyhaotian@gmail.com>
2025-09-11 02:11:36 -0400

    Restructure calls, type error remain

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,45 @@
 // API request
-async function fetchCongressTrades() {
-  try {
-    console.log(`Making request to: ${endpointUrl}`);
-    const response = await axios.get<any[]>(endpointUrl, config);
+// async function fetchCongressTrades() {
+//   try {
+//     console.log(`Making request to: ${endpointUrl}`);
+//     const response = await axios.get<any[]>(endpointUrl, config);
+//
+//     const targetMonth = "2025-08";
+//     const allTrades = response.data;
+//     const monthlyTrades = allTrades.filter(trade => {
+//       return trade.TransactionDate.startsWith(targetMonth);
+//     });
+//     console.log(`Found ${monthlyTrades.length} trades in ${targetMonth}`);
+//     console.log("-----------------------------------");
+//     // Get first 5
+//     const firstFiveTrades = response.data.slice(0, 50);
+//     // Loop through each trade and print it
+//     firstFiveTrades.forEach((trade, index) => {
+//       console.log(`Trade #${index + 1}:`);
+//       console.log(trade);
+//       console.log('---');
+//     });
+//     // // Total trade received
+//     console.log(`Total trade: ${response.data.length}`);
+//
+//
+//   } catch (error) {
+//     console.error("Error fetching data");
+//     if (axios.isAxiosError(error)) {
+//       console.log(`Status Code: ${error.response?.status}`);
+//       console.log(`Error Message: ${error.response?.data || error.message}`);
+//     } else {
+//       console.log("An unexpeceted error occurred: ", error);
+//     }
+//   }
+// }
+// await fetchCongressTrades();
 
-    const targetMonth = "2025-08";
-    const allTrades = response.data;
-    const monthlyTrades = allTrades.filter(trade => {
-      return trade.TransactionDate.startsWith(targetMonth);
-    });
-    console.log(`Found ${monthlyTrades.length} trades in ${targetMonth}`);
-    console.log("-----------------------------------");
-    // Get first 5
-    const firstFiveTrades = response.data.slice(0, 50);
-    // Loop through each trade and print it
-    firstFiveTrades.forEach((trade, index) => {
-      console.log(`Trade #${index + 1}:`);
-      console.log(trade);
-      console.log('---');
-    });
-    // // Total trade received
-    console.log(`Total trade: ${response.data.length}`);
+import { getCongressTradeData } from "./quiver/daily-trades";
 
+//
+async function main() {
+  const data = getCongressTradeData()
 
-  } catch (error) {
-    console.error("Error fetching data");
-    if (axios.isAxiosError(error)) {
-      console.log(`Status Code: ${error.response?.status}`);
-      console.log(`Error Message: ${error.response?.data || error.message}`);
-    } else {
-      console.log("An unexpeceted error occurred: ", error);
-    }
-  }
+  console.log("CongressTradeData: ", data)
 }
-await fetchCongressTrades();
-
-
--- src/quiver/daily-trades.ts
+++ src/quiver/daily-trades.ts
@@ -1,11 +1,14 @@
-import { type CongressTrade } from "./types";
-import { quiverConfig, ENDPOINTS } from "./api_client";
+import { CongressTradeSchema, type CongressTrade } from "./types";
+import { QUIVER_CONFIG, ENDPOINTS } from "./api_client";
 
-export function getTodayDate(): string {
-  return new Date().toISOString();
+export async function getCongressTradeData(): Promise<CongressTrade> {
+  console.log("Fetching raw data from CongressTrade api");
+
+  const response = await fetch(ENDPOINTS.CONGRESS_TRADING, {
+    headers: QUIVER_CONFIG.headers
+  })
+  const rawData = await response.json();
+  return CongressTradeSchema.parse(rawData)
 }
 
-export async function getTodaysTrades(): Promise<CongressTrade[]> {
-  console.log("Fetching trades from Quiver API")
 
-}

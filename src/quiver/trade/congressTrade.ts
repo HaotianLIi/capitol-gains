e6baeaca135eb22e9c5c19d9bb959789467ef85c
Frankie <frankie@AI.ottawa.ad.algonquincollege.com>
2025-09-24 20:20:51 -0400

    Using specific params of the api to get exact pages and version

--- src/quiver/trade/congressTrade.ts
+++ src/quiver/trade/congressTrade.ts
@@ -1,8 +1,43 @@
 import { CongressTradeArraySchema, type CongressTrade } from "../types";
 import { QUIVER_CONFIG, ENDPOINTS } from "../api_client";
+import { resolve } from "bun";
+
+
+export async function verifyPagination() {
+  console.log('Testing pagination with different page sizses')
+
+  const testSizes = [10000];
+  const allData: CongressTrade[] = [];
+  for (const pageSize of testSizes) {
+    const url = `https://api.quiverquant.com/beta/bulk/congresstrading?=page=1&page_size=${pageSize}&version=V2`;
+    try {
+
+      const response = await fetch(url, { headers: QUIVER_CONFIG.headers });
+      const data = await response.json() as CongressTrade[];
+      console.log(`PageSize: ${pageSize} -> Received: ${data.length} items`);
+
+      allData.push(...data);
+
+      //Save to file for inspection
+      const fs = require('fs');
+      fs.writeFileSync(`page_${pageSize}_data_V2.txt`,
+        `Version: V2 PageSize: ${pageSize}\nItems: ${data.length}\n\n${JSON.stringify(data, null, 2)}`
+      );
+      await new Promise(resolve => setTimeout(resolve, 200));
+    } catch (error) {
+      console.log('API error', error);
+    }
+
+    console.log(`\nTotal items across all page sizses: ${allData.length}`);
+  }
+
+}
+await verifyPagination();
+
 
 export async function getCongressTradeData(): Promise<CongressTrade[]> {
-  const response = await fetch(ENDPOINTS.CONGRESS_TRADING, {
+  const url = ENDPOINTS.congressTrading();
+  const response = await fetch(url, {
     headers: QUIVER_CONFIG.headers
   })
   if (!response.ok) {
@@ -26,4 +61,20 @@ export async function getCongressTradeData(): Promise<CongressTrade[]> {
   }
   const parsedData = CongressTradeArraySchema.parse(rawData);
   return parsedData;
+}
+
+export async function getCongressTradeByTicket(ticker: string) {
+  const url = ENDPOINTS.congressTradingByTicket(ticker);
+  console.log("URL", url);
+  const response = await fetch(url, {
+    headers: QUIVER_CONFIG.headers,
+  })
+  if (!response.ok) {
+    throw new Error(`API error of ${url}, StatusCode: ${response.status},Message: ${response.statusText}`)
+  }
+
+  const data = await response.json();
+  console.log("Get trade by ticker: ", data)
+  // console.log(`There are ${data.length} group of trade displayed`);
+  return data;
 }

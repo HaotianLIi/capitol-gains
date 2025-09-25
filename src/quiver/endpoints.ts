7074b4d327a8b9f1166dc86c6f5ef3b42b908ad9
Frankie <frankie@AI.ottawa.ad.algonquincollege.com>
2025-09-25 12:15:55 -0400

    update endpoints

---
+++ src/quiver/endpoints.ts
@@ -0,0 +1,23 @@
+const API_KEY = Bun.env.QUIVER_API_KEY;
+if (!API_KEY) throw new Error("QUIVER_API_KEY not found");
+
+export const QUIVER_CONFIG = {
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${API_KEY}`
+  }
+}
+
+export const ENDPOINTS = {
+  congressTradeByDefault: () =>
+    `https://api.quiverquant.com/beta/bulk/congresstrading`,
+  congressTradingBySize: (pageSize: number) =>
+    `https://api.quiverquant.com/beta/bulk/congresstrading?=page=1&page_size=${pageSize}&version=V2`,
+  congressTradingByTicket: (ticker: string) =>
+    `https://api.quiverquant.com/beta/historical/congresstrading/${ticker}`,
+  congressTradingByBioId: (bioGuideId: string) =>
+    `https://api.quiverquant.com/beta/bulk/congresstrading?=bioguide_id=${bioGuideId}`
+}
+
+
+

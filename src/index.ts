0940ce17c7b42402d48c8cf716e0479a64a3445a
Frankie <frankie@AI.ottawa.ad.algonquincollege.com>
2025-09-24 21:44:11 -0400

    testing page_size parameters with however trades you assigned with and write it in a file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-async function main() {
+import { verifyPagePrams } from "./quiver/trade/congressTrade";
 
+async function main() {
+  const createFileWith50Trade = verifyPagePrams(50);
+  return createFileWith50Trade;
 }
 
 await main();
